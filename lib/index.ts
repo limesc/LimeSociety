1249b30c0142416a7c559c32e993781266dd4163
myys0508 <myys0508@limesociety.com>
2019-02-13 18:45:27 +0900

    revuit 이동

--- lib/index.ts
+++
@@ -1,5 +0,0 @@
-export const getViewer = (ctx) => {
-  const { reduxStore } = ctx
-  const viewer = reduxStore ? reduxStore.getState().viewer : null
-  return viewer
-}
---
+++ nuxt.config.ts
@@ -0,0 +1,32 @@
+// import VuetifyLoaderPlugin from 'vuetify-loader/lib/plugin'
+
+const config = {
+  head: {
+    title: 'revuit',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui' },
+      { hid: 'description', name: 'description', content: 'revuit project' }
+    ],
+    link: [
+      { rel: 'shortcut icon', href: '/favicon.ico' },
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700,900|Material+Icons' },
+      { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/vuetify/dist/vuetify.min.css' }
+    ]
+  },
+  /* build: {
+    plugins: [new VuetifyLoaderPlugin()],
+    extractCSS: true,
+    transpile: ['vuetify/lib']
+  }, */
+  plugins: [
+    // '~/plugins/hello',
+    '~/plugins/vuetify'
+  ],
+  css: [
+    // '~/assets/app.styl',
+    // 'vuetify/dist/vuetify.min.css'
+  ]
+}
+
+export default config
