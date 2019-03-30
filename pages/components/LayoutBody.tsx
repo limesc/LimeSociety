fb2014d85b4b23e6ad5460622862d4ca00b7e5fb
Hojong Lee <hojonglee@limesociety.com>
2019-03-30 14:37:37 +0900

    js to tsx : components/LayoutBody

---
+++ pages/components/LayoutBody.tsx
@@ -0,0 +1,136 @@
+import { createStyles, WithStyles, withStyles } from '@material-ui/core'
+import { capitalize } from '@material-ui/core/utils/helpers'
+import clsx from 'clsx'
+import React from 'react'
+
+function round (value) {
+  return Math.round(value * 1e4) / 1e4
+}
+
+const styles = theme =>
+  createStyles({
+    margin: {
+      margin: theme.spacing.unit * 7
+    },
+    marginBottom: {
+      marginBottom: theme.spacing.unit * 12
+    },
+    widthSmall: {
+      width: 'auto',
+      marginLeft: theme.spacing.unit * 3,
+      marginRight: theme.spacing.unit * 3,
+      [theme.breakpoints.up(660 + theme.spacing.unit * 6)]: {
+        width: 660,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      }
+    },
+    widthMedium: {
+      width: 'auto',
+      marginLeft: theme.spacing.unit * 3,
+      marginRight: theme.spacing.unit * 3,
+      [theme.breakpoints.up(850 + theme.spacing.unit * 6)]: {
+        width: 850,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      }
+    },
+    widthLarge: {
+      width: 'auto',
+      marginLeft: theme.spacing.unit * 3,
+      marginRight: theme.spacing.unit * 3,
+      [theme.breakpoints.up('md')]: {
+        width: 880,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      },
+      [theme.breakpoints.up(round(880 / 0.7777))]: {
+        width: '77.7777%'
+      },
+      [theme.breakpoints.up(round(1400 / 0.7777))]: {
+        width: 1400
+      }
+    },
+    widthXlarge: {
+      width: 'auto',
+      marginLeft: theme.spacing.unit * 3,
+      marginRight: theme.spacing.unit * 3,
+      [theme.breakpoints.up('md')]: {
+        width: 900,
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      },
+      [theme.breakpoints.up(round(900 / 0.9))]: {
+        width: '90%'
+      },
+      [theme.breakpoints.up(round(1800 / 0.9))]: {
+        width: 1800
+      }
+    },
+    widthFull: {
+      width: '100%'
+    },
+    fullHeight: {
+      height: '100%'
+    }
+  })
+
+interface LayoutBodyOwnProps {
+  className?: string
+  component?: string | (new (props: any) => React.Component)
+  fullHeight?: boolean
+  fullWidth?: boolean
+  margin?: boolean
+  marginBottom?: boolean
+  style?: React.CSSProperties
+  width?: string | 'small' | 'medium' | 'large' | 'xlarge' | 'full'
+}
+type LayoutBodyProps = LayoutBodyOwnProps & WithStyles<typeof styles>
+
+class LayoutBody extends React.Component<LayoutBodyProps> {
+  static defaultProps = {
+    component: 'div',
+    fullHeight: false,
+    fullWidth: false,
+    margin: false,
+    marginBottom: false,
+    width: 'medium'
+  }
+
+  render () {
+    const {
+      children,
+      classes,
+      className,
+      component: Component,
+      fullHeight,
+      fullWidth,
+      margin,
+      marginBottom,
+      style,
+      width,
+      ...other
+    } = this.props
+
+    return (
+      <Component
+        className={clsx(
+          // classes.root,
+          {
+            [classes[`width${capitalize(width)}`]]: !fullWidth,
+            [classes.fullHeight]: fullHeight,
+            [classes.margin]: margin,
+            [classes.marginBottom]: marginBottom
+          },
+          className
+        )}
+        style={style}
+        {...other}
+      >
+        {children}
+      </Component>
+    )
+  }
+}
+
+export default withStyles(styles)(LayoutBody)
