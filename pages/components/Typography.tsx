97518c6941d57daa5c39b82e493ca4c086c735ab
Hojong Lee <hojonglee@limesociety.com>
2019-03-30 11:46:24 +0900

    js to tsx : components/Typography

---
+++ pages/components/Typography.tsx
@@ -0,0 +1,69 @@
+import { Typography as MuiTypography } from '@material-ui/core'
+import { createStyles, WithStyles, withStyles } from '@material-ui/core/styles'
+import { capitalize } from '@material-ui/core/utils/helpers'
+import React from 'react'
+
+const styles = theme =>
+  createStyles({
+    markedH2Center: {
+      height: 4,
+      width: 73,
+      display: 'block',
+      margin: `${theme.spacing.unit * 1}px auto 0`,
+      backgroundColor: theme.palette.secondary.main
+    },
+    markedH3Center: {
+      height: 4,
+      width: 55,
+      display: 'block',
+      margin: `${theme.spacing.unit * 1}px auto 0`,
+      backgroundColor: theme.palette.secondary.main
+    },
+    markedH4Center: {
+      height: 4,
+      width: 55,
+      display: 'block',
+      margin: `${theme.spacing.unit * 1}px auto 0`,
+      backgroundColor: theme.palette.secondary.main
+    },
+    markedH6Left: {
+      height: 2,
+      width: 28,
+      display: 'block',
+      marginTop: theme.spacing.unit * 0.5,
+      background: 'currentColor'
+    }
+  })
+
+interface TypographyProps extends WithStyles<typeof styles> {
+  marked?: boolean | 'center' | 'left'
+  variant?: any
+}
+
+class Typography extends React.Component<TypographyProps> {
+  static defaultProps = {
+    marked: false
+  }
+
+  render () {
+    const { children, classes, marked, variant, ...other } = this.props
+
+    return (
+      <MuiTypography variant={variant} {...other}>
+        {children}
+        {marked ? (
+          <span
+            className={
+              classes[
+                `marked${capitalize(variant) +
+                  capitalize(typeof marked === 'string' ? marked : '')}`
+              ]
+            }
+          />
+        ) : null}
+      </MuiTypography>
+    )
+  }
+}
+
+export default withStyles(styles)(Typography)
