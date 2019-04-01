42b08e73e2e7879d1bf67a07b929259934721d0a
Hojong Lee <hojonglee@limesociety.com>
2019-04-01 14:29:37 +0900

    Blog 수정

--- pages/index/components/Blog.tsx
+++ pages/index/components/Blog.tsx
@@ -11,25 +11,25 @@ import Container from './Container'
 
 const styles = theme =>
   createStyles({
+    background: {
+      backgroundColor: '#EFFBEF',
+      overflow: 'hidden'
+    },
     root: {
+      marginTop: theme.spacing.unit * 10,
+      marginBottom: theme.spacing.unit * 10,
       display: 'flex',
       flexDirection: 'column',
-      alignItems: 'center',
-      marginTop: theme.spacing.unit * 20,
-      marginBottom: theme.spacing.unit * 10
+      alignItems: 'center'
     },
     button: {
       border: '4px solid currentColor',
       borderRadius: 0,
       height: 'auto',
-      padding: theme.spacing.unit * (2, 5)
-    },
-    link: {
-      marginTop: theme.spacing.unit * 3,
-      marginBottom: theme.spacing.unit * 3
-    },
-    buoy: {
-      width: 60
+      paddingLeft: theme.spacing.unit * 5,
+      paddingRight: theme.spacing.unit * 5,
+      paddingTop: theme.spacing.unit * 2,
+      paddingBottom: theme.spacing.unit * 2
     }
   })
 
@@ -40,12 +40,14 @@ class Blog extends React.Component<BlogProps> {
     const { classes } = this.props
 
     return (
-      <Container className={classes.root} component='section'>
-        <Button className={classes.button} href='/blog'>
-          <Typography variant='h6' component='span'>
-            go to blog
-          </Typography>
-        </Button>
+      <Container className={classes.background} id='blog' width='full'>
+        <Container className={classes.root} component='section'>
+          <Button className={classes.button} href='/blog'>
+            <Typography variant='h6' component='span'>
+              go to blog
+            </Typography>
+          </Button>
+        </Container>
       </Container>
     )
   }
