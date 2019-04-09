39ccab7889fe71b7ec302fcbc5c7b0dfb8b3b0f1
Hojong Lee <hojonglee@limesociety.com>
2019-04-09 09:13:00 +0900

    임시 커밋

--- pages/blog/components/Post.tsx
+++ pages/blog/components/Post.tsx
@@ -6,7 +6,7 @@ import {
 } from '@material-ui/core'
 import React from 'react'
 
-import Markdown from './markdown'
+import Markdown from './TmpMarkdown'
 
 const styles = theme =>
   createStyles({
