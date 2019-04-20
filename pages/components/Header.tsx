69c10290292acc91553b0084b9978216dda8f4f8
Hojong Lee <hojonglee@limesociety.com>
2019-04-20 18:51:01 +0900

    블로그 헤더를 대문처럼 넓은 container 와 좁은 container 복합으로 만들기

--- pages/components/Header.tsx
+++ pages/components/Header.tsx
@@ -7,30 +7,21 @@ import {
 } from '@material-ui/core'
 import React from 'react'
 
-import { lightLimeColor } from 'consts/layout'
 import Container from './Container'
 
 const styles = theme =>
   createStyles({
     background: {
-      backgroundColor: lightLimeColor,
+      backgroundImage: 'url("/static/index/components/header-bg.jpg")',
+      color: theme.palette.common.white,
       overflow: 'hidden'
     },
     root: {
-      marginTop: theme.spacing.unit * 4,
-      marginBottom: theme.spacing.unit * 4,
+      marginTop: theme.spacing.unit * 10,
+      marginBottom: theme.spacing.unit * 10,
       [theme.breakpoints.down('xs')]: {
-        margin: 0
-      }
-    },
-    container: {
-      backgroundImage: 'url("/static/index/components/header-bg.jpg")',
-      color: theme.palette.common.white
-    },
-    content: {
-      padding: `${theme.spacing.unit * 6}px`,
-      [theme.breakpoints.up('md')]: {
-        paddingRight: 0
+        marginTop: theme.spacing.unit * 4,
+        marginBottom: theme.spacing.unit * 4
       }
     }
   })
@@ -49,21 +40,17 @@ class Header extends React.Component<HeaderProps> {
     return (
       <Container className={classes.background} width='full'>
         <Container className={classes.root} component='section'>
-          <Grid container className={classes.container}>
-            <Grid item md={6} className={classes.content}>
-              <Typography
-                color='inherit'
-                component={'h2' as any}
-                gutterBottom
-                variant='h3'
-              >
-                {title}
-              </Typography>
-              <Typography color='inherit' paragraph variant='body1'>
-                {content}
-              </Typography>
-            </Grid>
-          </Grid>
+          <Typography
+            color='inherit'
+            component={'h2' as any}
+            gutterBottom
+            variant='h3'
+          >
+            {title}
+          </Typography>
+          <Typography color='inherit' paragraph variant='body1'>
+            {content}
+          </Typography>
         </Container>
       </Container>
     )
