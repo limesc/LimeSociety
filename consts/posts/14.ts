1228dd5e52ac1f542a130c1a2b1bf294a2db60bc
Hojong Lee <hojonglee@limesociety.com>
2019-04-22 11:29:47 +0900

    학회 참석 글 넣기

---
+++ consts/posts/14.ts
@@ -0,0 +1,25 @@
+const post: IPost = {
+  title: '대한대장항문학회 제 52차 학술대회 부스 참여',
+  createdAt: 'Apr 5, 2019',
+  thumbnail: '/static/blog/post-14-2.jpg',
+  content: `
+2019년 4월 5일(금)부터 2019년 4월 7일(일)까지 제주도에서 열린 대한대장항문학회 제 52차 학술대회에 부스로 참석하였습니다.
+
+라임소사이어티는 2018년부터 대한대장항문학회와 Colorectal Cancer Web Database 구축 사업을 진행하고 있습니다.
+현재 1차년도 사업을 마무리해가는 과정에 있습니다.
+
+&nbsp;  
+<img src="/static/blog/post-14-1.jpg" alt="대한대장항문학회 제 52차 학술대회 부스 참여 (1)" width="100%" />
+&nbsp;  
+
+500명이 넘는 분들이 사전예약하셨으며, 성공적으로 마무리되었습니다.
+
+&nbsp;  
+<img src="/static/blog/post-14-2.jpg" alt="대한대장항문학회 제 52차 학술대회 부스 참여 (2)" width="100%" />
+&nbsp;  
+
+감사합니다.
+`
+}
+
+export default post
